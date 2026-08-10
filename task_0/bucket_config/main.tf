2dc889b1306eb5ab71bfc3f82b8c4f1dd418cd52
Mephistopheles <pyaro@softserveinc.com>
2026-08-10 18:47:47 +0300

    Initialised state storage

---
+++ task_0/bucket_config/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  profile = "terraform"
+}
+
+resource "aws_s3_bucket" "state" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_public_access_block" "state_access_rules" {
+  bucket = aws_s3_bucket.state.id
+
+  restrict_public_buckets = true
+  block_public_acls       = true
+  block_public_policy     = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption" {
+  bucket = aws_s3_bucket.state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "state_versioning" {
+  bucket = aws_s3_bucket.state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
