56e3eba6137cddec6ddac65981cfdd6a856a2aff
Mephistopheles <pyaro@softserveinc.com>
2026-08-13 21:05:33 +0300

    Provisioned security groups

--- task_1/infrastructure/main.tf
+++ task_1/infrastructure/main.tf
@@ -40,3 +40,59 @@ module "routing" {
 
   enable_nat = false
 }
+
+module "bastion_security" {
+  source = "./modules/security"
+
+  name        = "bastion-sg"
+  description = "Bastion security group"
+  vpc_id      = module.vpc.vpc_id
+
+  rules = {
+    ssh = {
+      description        = "ssh conection rule"
+      traffic_type       = "ingress"
+      source_cidr_blocks = [var.my_ip]
+      port_range_start   = 22
+      port_range_end     = 22
+      protocol_name      = "tcp"
+    }
+  }
+}
+
+module "asg_security" {
+  source = "./modules/security"
+
+  name        = "scaling-sg"
+  description = "Auto scalling group security rules"
+  vpc_id      = module.vpc.vpc_id
+
+  rules = {
+    ssh = {
+      description        = "ssh connection from bastion"
+      traffic_type       = "ingress"
+      source_security_group_id = module.bastion_security.security_group_id
+      port_range_start   = 22
+      port_range_end     = 22
+      protocol_name      = "tcp"
+    }
+  }
+}
+
+module "rds_security" {
+  source = "./modules/security"
+  name = "database-sg"
+  description = "Database instances security group"
+  vpc_id = module.vpc.vpc_id
+
+  rules = {
+    ssh = {
+      description = "ssh connection from bastion"
+      traffic_type = "ingress"
+      source_security_group_id = module.bastion_security.security_group_id
+      port_range_start = 22
+      port_range_end = 22
+      protocol_name = "tcp"
+    }
+  }
+}
