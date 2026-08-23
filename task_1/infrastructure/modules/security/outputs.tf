c9190f4c493f965d6855728abcfbaff46dd8120d
Mephistopheles <pyaro@softserveinc.com>
2026-08-23 23:00:27 +0300

    Updated security module

--- task_1/infrastructure/modules/security/outputs.tf
+++ task_1/infrastructure/modules/security/outputs.tf
@@ -1,15 +1,23 @@
 output "security_group_id" {
-  value = aws_security_group.guard.id
+  value = {
+    for name, group in aws_security_group.guard : name => group.id
+  }
 }
 
 output "security_group_name" {
-  value = aws_security_group.guard.name
+  value = {
+    for name, group in aws_security_group.guard : name => group.name
+  }
 }
 
 output "security_group_arn" {
-  value = aws_security_group.guard.arn
+  value = {
+    for name, group in aws_security_group.guard : name => group.arn
+  }
 }
 
 output "security_group_vpc_id" {
-  value = aws_security_group.guard.vpc_id
+  value = {
+    for name, group in aws_security_group.guard : name => group.vpc_id
+  }
 }
--- task_1/infrastructure/modules/security/variables.tf
+++ task_1/infrastructure/modules/security/variables.tf
@@ -1,23 +1,18 @@
-variable "name" {
-  type = string
-}
-
-variable "description" {
-  type = string
-}
-
 variable "vpc_id" {
   type = string
 }
 
-variable "rules" {
+variable "security_groups" {
   type = map(object({
-    traffic_type             = optional(string, "ingress")
-    rule_description         = optional(string, "")
-    source_security_group_id = optional(string)
-    source_cidr_blocks       = optional(list(string))
-    port_range_start         = number
-    port_range_end           = number
-    protocol_name            = optional(string, "tcp")
+    group_description = string
+    rules = map(object({
+      traffic_type             = optional(string, "ingress")
+      description              = optional(string, "")
+      source_security_group_id = optional(string)
+      source_cidr_blocks       = optional(list(string))
+      port_range_start         = number
+      port_range_end           = number
+      protocol_name            = optional(string, "tcp")
+    }))
   }))
 }
